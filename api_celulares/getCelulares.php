762eec1bd671278c0754a215a18a36decfaf93ea
lizeth-sal <lizeth.salcido23@cetis107.edu.mx>
2025-06-23 21:55:46 -0700

    Update getCelulares.php

--- api_celulares/getCelulares.php
+++ api_celulares/getCelulares.php
@@ -1,30 +1,27 @@
 <?php
 
-    $host = '192.localhost';
+    $host = 'localhost';
     $user = 'root';
     $pass = '';
     $dbname = 'celularesdb';
 
-    //crear conexión
     $conn = new mysqli('localhost', 'root', '', 'celularesdb');
 
-    //verificar conexión
     if($conn->connect_error){
         http_response_code(500);
-        echo json_encode(['error' => 'Error de cpnexión: ' . $conn->connect_error]);
+        echo json_encode(['error' => 'Error de conexión: ' . $conn->connect_error]);
         exit;
     }
 
-    //consulta mysql
 
     $sql = "SELECT * FROM celulares";
     $result= $conn->query($sql);
 
-    $actors = [];
+    $celulares = [];
 
     if($result && $result->num_rows > 0){
         while($row = $result->fetch_assoc()){
-            $actors[] = $row;
+            $celulares[] = $row;
         }
     }
 
@@ -33,7 +30,8 @@
 
 
     header("Content-Type: application/json");
-    echo json_encode($actors);
+    echo json_encode($celulares);
 
 
 ?>
+
